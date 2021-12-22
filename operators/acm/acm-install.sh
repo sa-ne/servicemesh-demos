3be253d7a5afc0ce5b1f50c72570a29f940a1d5e
MoOyeg <mojojoye@yahoo.com>
2021-12-21 23:35:26 -0500

    updates

--- operators/acm/acm-install.sh
+++ operators/acm/acm-install.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
 GITOPS_LINK="${GITOPS_LINK:=https://github.com/redhat-cop/gitops-catalog.git}"
 PULL_SECRET_LOCATION="${PULL_SECRET_LOCATION:-}"
 CLONE_FOLDER="${GITOPS_CLONE_FOLDER:=/tmp/gitops}"
 CHANNEL="${ACM_CHANNEL:=release-2.4}"
 ACM_NAMESPACE="${ACM_NAMESPACE:=open-cluster-management}"
-PULL_SECRET_NAME="${PULL_SECRET_NAME:=pull-secret}"
+PULL_SECRET_NAME="${PULL_SECRET_NAME:=multiclusterhub-operator-pull-secret}"
+MULTICLUSTER_OBSERVABILIITY="true"
 
 function clone () {
 # Provide $1 link to clone
@@ -23,6 +24,7 @@ function check_secret_exists() {
 }
 
 #Clone Gitops Repo
+echo "Cloning Gitops Repo"
 clone $GITOPS_LINK $CLONE_FOLDER
 
 #Create ACM Operator
@@ -77,3 +79,55 @@ fi
 
 
 
+if [[ $MULTICLUSTER_OBSERVABILIITY = "true" ]]
+then
+    
+    #Make Sure Multi-ClusterHub Exists First
+    until [ $(oc get multiclusterhub -n open-cluster-management -o jsonpath='{.items[0].status.phase}') = "Running" ];do \
+    echo "Creating MultiClusterHub" && sleep 5;done
+    
+    echo "Export your Variables for Object Storage, see thanos-object-storage.yaml for environment variables"
+
+    #Create Observability Namespace
+    oc create namespace open-cluster-management-observability
+
+    #Try obtain pull secret
+    DOCKER_CONFIG_JSON=`oc extract secret/multiclusterhub-operator-pull-secret -n open-cluster-management --to=-`
+    secret_status=$?
+    if [[ $secret_status -eq 0 ]] || [[ $DOCKER_CONFIG_JSON == "" ]]
+    then
+      DOCKER_CONFIG_JSON=`oc extract secret/pull-secret -n openshift-config --to=-`
+      secret_status=$?
+      if [[ $secret_status -eq 1 ]]
+      then
+        echo "Could not get pull secret to create"
+        exit 1
+      fi
+      oc create secret generic multiclusterhub-operator-pull-secret \
+      -n open-cluster-management-observability \
+      --from-literal=.dockerconfigjson="$DOCKER_CONFIG_JSON" \
+      --type=kubernetes.io/dockerconfigjson
+    fi
+
+    #Create object storage secret, make sure values are set via env variables, see thanos object storage
+    echo "Create object storage secret, make sure values are set via env variables, see thanos object storage"
+    envsubst < ./operators/acm/thanos-object-storage.yaml | oc create -f -
+
+    #Create the multicluster observability object
+    echo "Create the multicluster observability object"
+    echo """
+apiVersion: observability.open-cluster-management.io/v1beta2
+kind: MultiClusterObservability
+metadata:
+  name: observability
+spec:
+  observabilityAddonSpec: {}
+  storageConfig:
+    metricObjectStorage:
+      name: thanos-object-storage
+      key: thanos.yaml
+""" | oc create -f -
+
+
+
+fi
